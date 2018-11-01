519e56afdfc10d6624f0013b1e98c413f73cf8f7
Joel Chamakala <jchamakala9074@jh-mba158-04.d214.org>
2018-11-01 11:28:00 -0500

    Initial Commit

---
+++ IntergalacticTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntergalacticTravel
+//
+//  Created by Joel Chamakala on 11/1/18.
+//  Copyright © 2018 Joel Chamakala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
